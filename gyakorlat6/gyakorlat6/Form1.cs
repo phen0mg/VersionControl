48bb5271e9a82971ef522ca57599de50c2b7fd57
Levente Nagy <levente.nagy6@stud.uni-corvinus.hu>
2020-10-18 16:13:34 +0200

    Exchange

--- gyakorlat6/gyakorlat6/Form1.cs
+++ gyakorlat6/gyakorlat6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyakorlat6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,18 @@ namespace gyakorlat6
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
