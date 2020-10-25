4133d2058849e7c342615f597d36bb4a912c62cd
Levente Nagy <levente.nagy6@stud.uni-corvinus.hu>
2020-10-25 17:02:48 +0100

    osztályok

---
+++ gyakorlat7_CR9ZJY/gyakorlat7_CR9ZJY/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorlat7_CR9ZJY.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Probability { get; set; }
+
+    }
+}
