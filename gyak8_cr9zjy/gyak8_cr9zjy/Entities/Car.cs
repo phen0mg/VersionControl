829281a02da045eb8694c9a0ad18da7cfe1a19d8
Levente Nagy <levente.nagy6@stud.uni-corvinus.hu>
2020-11-15 17:10:26 +0100

    car

---
+++ gyak8_cr9zjy/gyak8_cr9zjy/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using gyak8_cr9zjy.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak8_cr9zjy.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
