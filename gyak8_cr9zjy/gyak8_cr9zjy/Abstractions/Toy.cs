ffdd1c56b99cbfa2592747476be75a0286be4fd7
Levente Nagy <levente.nagy6@stud.uni-corvinus.hu>
2020-11-15 16:52:01 +0100

    abstract

---
+++ gyak8_cr9zjy/gyak8_cr9zjy/Abstractions/Toy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak8_cr9zjy.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
