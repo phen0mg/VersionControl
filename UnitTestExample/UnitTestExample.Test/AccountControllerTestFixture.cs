1bf33a5f434bdfdeba5fce7ca079a76704009337
Levente Nagy <levente.nagy6@stud.uni-corvinus.hu>
2020-11-22 21:02:02 +0100

    psw test

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -24,6 +24,23 @@ namespace UnitTestExample.Test
             var actualResult = accountController.ValidateEmail(email);
 
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+        Test,
+        TestCase("abCdefgh", false),
+        TestCase("ABCD1234", false),
+        TestCase("abcd1234", false),
+        TestCase("Ab1", false),
+        TestCase("Abcd1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
+
+            Assert.AreEqual(expectedResult, actualResult);
 
         }
     }
